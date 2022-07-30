db2a82553a1e19b31fce603684651740773fd96a
husam maher <barber.shop.manag@gmail.com>
2022-07-30 19:57:46 +0300

    boostrap

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom"
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from 'react-toastify';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import "./index.css"
 import { Header } from './components/Header';
 import Dashboard from './pages/Dashboard';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import HeaderB from './components/header-bootrap/headerB';
 
 function NotFound() {
   return (
@@ -19,8 +21,8 @@ function NotFound() {
 function App() {
   return (
     <>
-
-      <Router>
+      <HeaderB />
+      {/* <Router>
         <div className='container'>
           <Header />
           <Routes>
@@ -32,7 +34,7 @@ function App() {
 
         </div>
       </Router>
-      <ToastContainer />
+      <ToastContainer /> */}
     </>
   );
 }
